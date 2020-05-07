2a059b1f40856517dc84237f259dbaa302ae8b84
David Papamichael <davidpapamichael@gmail.com>
2020-05-07 17:54:29 +0100

    all tests substituted with station doubles

--- jasmine-3.5.0/spec/journeyLogSpec.js
+++ jasmine-3.5.0/spec/journeyLogSpec.js
@@ -1,35 +1,47 @@
 describe("JourneyLog", function () {
   let journeyLog;
+  let station;
+  let stationTwo;
 
   beforeEach(function () {
     journeyLog = new JourneyLog();
+
+    station = {
+      name: "Woodford",
+      zone: 1,
+    };
+
+    stationTwo = {
+      name: "Hampstead",
+      zone: 1,
+    };
   });
 
   describe("startLog", function () {
     it("logs the starting station", function () {
-      journeyLog._startLog("Woodford");
-      expect(journeyLog.currentJourney.startStation).toEqual("Woodford");
+      journeyLog._startLog(station);
+      expect(journeyLog.currentJourney.startStation.name).toEqual("Woodford");
     });
   });
 
   describe("endLog", function () {
     it("logs the end station", function () {
-      journeyLog._endLog("Hampstead");
-      expect(journeyLog.currentJourney.endStation).toBe("Hampstead");
+      journeyLog._endLog(stationTwo);
+      expect(journeyLog.currentJourney.endStation.name).toBe("Hampstead");
     });
   });
 
   describe("reset", function () {
     it("resets the current journey once completed", function () {
-      journeyLog._startLog("Woodford");
-      journeyLog._endLog("Hampstead");
+      journeyLog._startLog(station);
+      journeyLog._endLog(stationTwo);
       journeyLog._resetCurrentJourney();
       expect(journeyLog.history.length).toEqual(1);
     });
 
     it("resets the current journey once completed", function () {
-      journeyLog._startLog("Woodford");
-      journeyLog._endLog("Hampstead");
+      journeyLog._startLog(station);
+      journeyLog._endLog(stationTwo);
       journeyLog._resetCurrentJourney();
       expect(journeyLog.currentJourney.startStation).toBe("");
       expect(journeyLog.currentJourney.endStation).toBe("");
@@ -38,11 +50,11 @@ describe("JourneyLog", function () {
 
   describe("immuteJourney", function () {
     it("journey log cannot be changed", function () {
-      journeyLog._startLog("Woodford");
-      journeyLog._endLog("Hampstead");
+      journeyLog._startLog(station);
+      journeyLog._endLog(stationTwo);
       journeyLog._resetCurrentJourney();
       journeyLog.history[0].startStation = "Waterloo";
-      expect(journeyLog.history[0].startStation).toBe("Woodford");
+      expect(journeyLog.history[0].startStation.name).toBe("Woodford");
     });
   });
 });
--- jasmine-3.5.0/spec/journeySpec.js
+++ jasmine-3.5.0/spec/journeySpec.js
@@ -2,22 +2,34 @@
 
 describe("Journey", function () {
   let journey;
+  let station;
+  let stationTwo;
 
   beforeEach(function () {
     journey = new Journey();
+
+    station = {
+      name: "Woodford",
+      zone: 1,
+    };
+
+    stationTwo = {
+      name: "Hampstead",
+      zone: 1,
+    };
   });
 
   describe("startJourney", function () {
     it("logs the starting station", function () {
-      journey._startJourney("Woodford");
-      expect(journey.startStation).toBe("Woodford");
+      journey._startJourney(station);
+      expect(journey.startStation.name).toBe("Woodford");
     });
   });
 
   describe("endJourney", function () {
     it("logs the end station", function () {
-      journey._endJourney("Hampstead");
-      expect(journey.endStation).toBe("Hampstead");
+      journey._endJourney(stationTwo);
+      expect(journey.endStation.name).toBe("Hampstead");
     });
   });
 });
