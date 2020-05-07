c7f1775b76a22a1e384603fa672022d6851fcbf3
David Papamichael <davidpapamichael@gmail.com>
2020-05-07 11:59:09 +0100

    top up maximum added, throws error if reached

--- jasmine-3.5.0/spec/travelCardSpec.js
+++ jasmine-3.5.0/spec/travelCardSpec.js
@@ -9,18 +9,31 @@ describe("TravelCard", function () {
     it("it starts with a balance of £0", function () {
       expect(travelCard.balance).toEqual(0);
     });
+
+    it("has a maximum balance of £90", function () {
+      expect(travelCard.maxBalance).toEqual(90);
+    });
   });
 
   describe("addMoney", function () {
     it("can add money to card", function () {
-      travelCard.addMoney(20);
+      travelCard.topUp(20);
       expect(travelCard.balance).toEqual(20);
     });
 
     it("provides a message displaying the amount added and the new total balance", function () {
-      travelCard.addMoney(20);
-      expect(travelCard.addMoney(20)).toEqual(
+      travelCard.topUp(20);
+      expect(travelCard.topUp(20)).toEqual(
         "£20 Sucessfully added, Balance = £40"
+      );
+    });
+
+    it("throws an error if max balance is exceeded during topUp", function () {
+      travelCard.topUp(80);
+      expect(function () {
+        travelCard.topUp(11);
+      }).toThrowError(
+        "Can Not Exceed Maximum balance of £90, Max Top Up Avaliable = £10"
       );
     });
   });
--- jasmine-3.5.0/src/travelCard.js
+++ jasmine-3.5.0/src/travelCard.js
@@ -1,10 +1,19 @@
 class TravelCard {
   constructor() {
     this.balance = 0;
+    this.maxBalance = 90;
   }
 
-  addMoney = (money) => {
-    this.balance += money;
-    return `£${money} Sucessfully added, Balance = £${this.balance}`;
+  topUp = (money) => {
+    if (this.balance + money <= this.maxBalance) {
+      this.balance += money;
+      return `£${money} Sucessfully added, Balance = £${this.balance}`;
+    } else {
+      throw new Error(
+        `Can Not Exceed Maximum balance of £${
+          this.maxBalance
+        }, Max Top Up Avaliable = £${this.maxBalance - this.balance}`
+      );
+    }
   };
 }
