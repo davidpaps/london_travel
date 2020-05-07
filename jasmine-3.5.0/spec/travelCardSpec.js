601912e655b89e1309c9b4ced30db36eb486f146
David Papamichael <davidpapamichael@gmail.com>
2020-05-07 11:47:57 +0100

    add money function implemented, adds funds and displays message

--- jasmine-3.5.0/spec/travelCardSpec.js
+++ jasmine-3.5.0/spec/travelCardSpec.js
@@ -6,8 +6,22 @@ describe("TravelCard", function () {
   });
 
   describe("balance", function () {
-    it("itstarts with a balance of £0", function () {
+    it("it starts with a balance of £0", function () {
       expect(travelCard.balance).toEqual(0);
+    });
+  });
+
+  describe("addMoney", function () {
+    it("can add money to card", function () {
+      travelCard.addMoney(20);
+      expect(travelCard.balance).toEqual(20);
+    });
+
+    it("provides a message displaying the amount added and the new total balance", function () {
+      travelCard.addMoney(20);
+      expect(travelCard.addMoney(20)).toEqual(
+        "£20 Sucessfully added, Balance = £40"
+      );
     });
   });
 });
--- jasmine-3.5.0/src/travelCard.js
+++ jasmine-3.5.0/src/travelCard.js
@@ -2,4 +2,9 @@ class TravelCard {
   constructor() {
     this.balance = 0;
   }
+
+  addMoney = (money) => {
+    this.balance += money;
+    return `£${money} Sucessfully added, Balance = £${this.balance}`;
+  };
 }
